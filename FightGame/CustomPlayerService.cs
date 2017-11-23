9db8b3d1a025814b173e41442bacac5666520cff
Desarrollo03 <francisco.yaguez@gmail.com>
2017-11-23 11:30:37 +0100

    allá que voy

---
+++ FightGame/CustomPlayerService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FightGame
+{
+    public interface IPlayerService
+    {
+        List<Player> GetPlayers();
+        {
+        return null;
+        }
+
+        Players = new List<Player>
+            {
+                
+                new Player
+                {
+                    Id = ++Game.LastId,
+                    Name = "Cat Woman",
+                    Gender = Gender.Female,
+                    Lives = DefaultLives,
+                    Power = DefaultPower
+    },
+                new Player
+                {
+                    Id = ++Game.LastId,
+                    Name = "Lobezno",
+                    Gender = Gender.Male,
+                    Lives = DefaultLives,
+                    Power = DefaultPower
+},
+                new Player
+                {
+                    Id = ++Game.LastId,
+                    Name = "Wonder Woman",
+                    Gender = Gender.Female,
+                    Lives = DefaultLives,
+                    Power = DefaultPower
+                },
+                new Player
+                {
+                    Id = ++Game.LastId,
+                    Name = "Batman",
+                    Gender = Gender.Male,
+                    Lives = DefaultLives,
+                    Power = DefaultPower
+                },
+            };
+
+    }
+
+    public class CustomPlayerService : IPlayerService
+    {
+        public List<Player> GetPlayers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FightGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FightGame
+{
+    // otro cambio
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            game.Run();
+        }
+    }
+}
